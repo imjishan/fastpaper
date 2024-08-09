e1cd41ded98c8038719c85f564d057e9f3c6e3b8
Jishan <129368844+imjishan@users.noreply.github.com>
2024-08-09 17:24:56 +0530

    Update blog-card-section.tsx

--- components/blog-card-section.tsx
+++ components/blog-card-section.tsx
@@ -6,11 +6,7 @@ import { Separator } from './ui/separator'
 export default function BlogCardSection() {
 
   return (
-    <div className='max-w-[700px] my-[2rem] w-full'>
-      <div className='flex flex-col items-start justify-center w-full mb-4 gap-2'>
-        <h1 className='scroll-m-20 text-xl font-medium tracking-tight'>Checkout These Blog Posts</h1>
-        <Separator />
-      </div>
+    <div className='max-w-[700px] my-[1rem] w-full'>
       <div className='grid sm:grid-cols-2 lg:grid-col:2 xl:grid-cols-2 gap-2'>
         {blogs?.map((blog) => (
           <Link href={blog?.link} key={blog?.id}>
